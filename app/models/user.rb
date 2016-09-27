d91d856b1fd6792b7fda9a3ed3e193a966947157
rails v <rtejagorti@unm.edu>
2016-09-27 12:40:06 -0600

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+         ROLES = %i[manager parent]
+
+           def roles= (roles)
+             roles= [*roles].map { |r| r.to_sym}
+             self.roles_mask = (roles & ROLES). map { |r| 2**ROLES.index(r) }.inject(0, :+)
+           end
+
+           def roles
+             ROLES.reject do |r|
+               ((roles_mask.to_i ||0) & 2**ROLES.index(r)).zero?
+             end
+           end
+
+           def has_role?(role)
+             roles.include?(role)
+           end
+
+
+end
